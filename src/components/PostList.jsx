4c91f53eb64110201f4a0aaba57218ad7fe3f7bc
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-13 20:15:26 +0300

    add remove post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PostItem from './PostItem'
 
-function PostList({posts, title}) {
+function PostList({posts, title, remove}) {
     return (
         <div className="main">
             <h2>{title}</h2>
 			{posts.map((post, index) =>
-				<PostItem number={index + 1} post={post} key={post.id} />
+				<PostItem remove={remove} number={index + 1} post={post} key={post.id} />
 			)}
         </div>
     )
