9b0b68ca82f2a7dcb9b8d336053ff95c44b3deae
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-13 17:03:01 +0300

    add PostItem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
-import ClassCounter from "./components/ClassCounter";
+import React, {useState} from "react";
+import PostItem from "./PostItem";
+import './styles/App.css'
 
 function App() {
+
+	const [posts, setPosts] = useState([
+		{id: 1, title: 'Javascript 1', body:'Description full news'},
+		{id: 2, title: 'Javascript 2', body:'Description full news'},
+		{id: 3, title: 'Javascript 3', body:'Description full news'}
+	])
     
     return (
         <div className="App">
-			<ClassCounter />
+			<h2>Список постов</h2>
+			{posts.map(post => 
+				<PostItem post={post} key={post.id} />
+			)}
         </div>
     );
 }
