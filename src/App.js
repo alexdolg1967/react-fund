5cd31e1780bd379e43abc7b2eefb923630310383
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-14 19:14:12 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import MyButton from "./components/UI/Button/MyButton";
 import { MyModal } from "./components/UI/Modal/MyModal";
 import { usePosts } from "./hooks/usePosts";
 
-
 import "./styles/App.css";
 import PostService from "./components/API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import { useFetching } from "./hooks/useFetching";
-import { getPageCount } from "./utils/page";
+import { getPageCount, getPagesArray } from "./utils/page";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -20,38 +19,38 @@ function App() {
         { id: 3, title: "Delphi 3", body: " news" },
     ]);
 
-	const [filter, setFilter] = useState({sort: '', query: ''})
-	const [modal, setModal] = useState(false)
-	const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
+    const [filter, setFilter] = useState({ sort: "", query: "" });
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
-	const [totalPages, setTotalPages] = useState(0)
-	const [limit, setLimit] = useState(10)
-	const [page, setPage] = useState(1)
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
 
-	let pagesArray = []
+    let pagesArray = getPagesArray(totalPages);
 
-	for (let i = 0; i < totalPages; i++) {
-		pagesArray.push(i)
-		
-	}
+    const changePage = (page) => {
+        setPage(page);
+        fetchPosts(limit, page);
+    };
 
-	const [fetchPosts, isPostsLoading, postError] = useFetching(async() => {
-		const response = await PostService.getAll(limit, page)
-			setPosts(response.data)
-			const totalCount = response.headers['x-total-count']
-			setTotalPages(getPageCount(totalCount, limit))
-	})
+    const [fetchPosts, isPostsLoading, postError] = useFetching(
+        async (limit, page) => {
+            const response = await PostService.getAll(limit, page);
+            setPosts(response.data);
+            const totalCount = response.headers["x-total-count"];
+            setTotalPages(getPageCount(totalCount, limit));
+        }
+    );
 
-	useEffect(() => {
-		fetchPosts()
-		return () => {
-			
-		}
-	}, [])
+    useEffect(() => {
+        fetchPosts(limit, page);
+        return () => {};
+    }, []);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
-		setModal(false)
+        setModal(false);
     };
 
     const removePost = (post) => {
@@ -60,35 +59,45 @@ function App() {
 
     return (
         <div className="app">
-			<hr />
-			<div className="search">
-				<MyButton onClick={() => setModal(true)}>
-					Создать пост
-				</MyButton>
-				<MyButton onClick={fetchPosts}>
-					Получить посты
-				</MyButton>
-			</div>
-			<hr />
-			<MyModal visible={modal} setVisible={setModal}>
-				<PostForm create={createPost} />
-			</MyModal>
-			<PostFilter 
-				filter={filter}
-				setFilter={setFilter}
-			/>
-			{postError &&
-				<h2>Произошла ошибка ${postError}</h2>
-			}
-			{isPostsLoading
-				? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader /></div>
-				: <PostList
-					remove={removePost}
-					posts={sortedAndSearchPosts}
-					title="Список постов 1"
-				/>
-			}
-			
+            <hr />
+            <div className="search">
+                <MyButton onClick={() => setModal(true)}>Создать пост</MyButton>
+                <MyButton onClick={fetchPosts}>Получить посты</MyButton>
+            </div>
+            <hr />
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost} />
+            </MyModal>
+            <PostFilter filter={filter} setFilter={setFilter} />
+            {postError && <h2>Произошла ошибка ${postError}</h2>}
+            {isPostsLoading ? (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "50px",
+                    }}
+                >
+                    <Loader />
+                </div>
+            ) : (
+                <PostList
+                    remove={removePost}
+                    posts={sortedAndSearchPosts}
+                    title="Список постов 1"
+                />
+            )}
+            <div className="pagination">
+                {pagesArray.map((p) => (
+                    <span
+                        key={p}
+                        onClick={() => changePage(p)}
+                        className={page === p ? "page current" : "page"}
+                    >
+                        {p}
+                    </span>
+                ))}
+            </div>
         </div>
     );
 }
